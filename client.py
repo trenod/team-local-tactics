d722177d816c0524457f7006dbcd81f3ee6acb12
Tore B <torewb@gmail.com>
2022-03-01 10:01:58 +0100

    Added some imports and ideas

---
+++ client.py
@@ -0,0 +1,4 @@
+from team-local-tactics import print_available_champs, input_champion, print_match_summary
+#import TCP stuff
+
+
---
+++ server.py
@@ -0,0 +1,8 @@
+#import TCP stuff
+
+
+
+
+#class for player with data on each player that updates with both clients:
+
+class player 
